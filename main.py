c797dd6f3b728a57931aa4695fd7c5336097a9c4
Cheran Senthil <cheran.v.senthil@gmail.com>
2018-07-24 17:36:00 +0530

    Improved criteria to accept challenge

--- main.py
+++ main.py
@@ -9,25 +9,27 @@ lichess = berserk.Client(session)
 
 for event in lichess.bots.stream_incoming_events():
     if event['type'] == 'challenge':
-        Chessboard = chaturanga.Chessboard()
-        game_id = event['challenge']['id']
-        lichess.bots.accept_challenge(game_id)
+        challenge = event['challenge']
+        if (challenge['variant']['key'] == 'standard') and (challenge['rated'] == False):
+            Chessboard = chaturanga.Chessboard()
+            game_id = challenge['id']
+            lichess.bots.accept_challenge(game_id)
 
-        for game_state in lichess.bots.stream_game_state(game_id):
-            if game_state['type'] == 'gameFull':
-                is_black = game_state['black']['id'] == bot_id
-                bot_color = {True: 'b', False: 'w'}[is_black]
+            for game_state in lichess.bots.stream_game_state(game_id):
+                if game_state['type'] == 'gameFull':
+                    is_black = game_state['black']['id'] == bot_id
+                    bot_color = {True: 'b', False: 'w'}[is_black]
 
-                if bot_color == 'w':
-                    bot_move = chaturanga.bot(Chessboard)[0]
-                    Chessboard.move(bot_move)
-                    lichess.bots.make_move(game_id, bot_move)
+                    if bot_color == 'w':
+                        bot_move = chaturanga.bot(Chessboard)[0]
+                        Chessboard.move(bot_move)
+                        lichess.bots.make_move(game_id, bot_move)
 
-            if game_state['type'] == 'gameState':
-                last_move = game_state['moves'].split(' ')[-1]
-                Chessboard.move(last_move)
+                if game_state['type'] == 'gameState':
+                    last_move = game_state['moves'].split(' ')[-1]
+                    Chessboard.move(last_move)
 
-                if bot_color == Chessboard.active_color:
-                    bot_move = chaturanga.bot(Chessboard)[0]
-                    Chessboard.move(bot_move)
-                    lichess.bots.make_move(game_id, bot_move)
+                    if bot_color == Chessboard.active_color:
+                        bot_move = chaturanga.bot(Chessboard)[0]
+                        Chessboard.move(bot_move)
+                        lichess.bots.make_move(game_id, bot_move)
